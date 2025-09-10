7d76bb5299eb7d5e90bcb7b0b7e1251009c7b450
Dany Smirnov <dany-s.smirnov2015@yandex.ru>
2025-09-10 21:07:22 +0300

    Основа

---
+++ CppMatrix.Tests/CppMatrix.Tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CppMatrixTests
+{
+	TEST_CLASS(CppMatrixTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
